1d6784bec2e1f64ecd147a3fa8cc512b81c13e40
Melbin23 <melbinpaulraj@gmail.com>
2025-04-07 10:06:13 +0530

    hello

---
+++ Melbin.py
@@ -0,0 +1,183 @@
+#a=9
+#b=4.56
+#c=a+b
+#print(c)
+#a=7
+#b="8"
+#c=int(b)
+#print(a+c)
+#a="hello world"
+#print(a[3])
+#a="hello world"
+#print(a[1:4])
+#a="melbin"
+#print(len(a))
+#z="HELLO WORLD"
+#print(z.lower())
+#g="hello world"
+#print(g.upper())
+#f="hello,world"
+#print(f.replace("world","melbin"))
+#a=input("enter your age")
+#b=input("how many year experience do you have?")
+"""c=int(a)
+d=int(b)
+print(c+d)"""
+#k="hello melbin"
+#print(k[0:6].upper())
+#a="vignesh"
+#print("hello"+ a)
+#a=4
+#print("hello"+a)
+#print(3+2.67)
+#print(3+4.77)
+#print("3"+4.77)
+#a=input("what is your name")
+#print("hello"+ a)
+#a="a"
+#print(type(a))
+#a=4
+#b=5.5
+#print(a+b)
+#q="45"
+"""z=78
+s=int(q)
+print(s+z)
+"""
+#a="hello"
+#print(a[0])
+#a="hello"
+#print(a[1:4])
+#a="hello melbin"
+#print(len(a))
+#a="HELLO"
+#print(a.lower())
+#a="hello"
+#print(a.upper())
+"""a="hello,world"
+print(a.replace("world","vignesh"))
+a="45"
+b=78
+c=int(a)
+print(c+b)"""
+#a={1,2,3,4,5,6}
+"""a.difference_update([3,4,7])
+print(a)
+a.add(4)
+print(a)
+a.update([7,8,9])
+print(a)
+a.remove(1)
+print(a)
+a.discard(2)
+a1=a.copy()
+print(a1)
+a1.clear()
+print(a,a1)"""
+"""a1={3,5,7,9}
+print(a1-a)
+print(a&a1)
+print(a|a1)"""
+A={'apple':2,'orange':3,'grapes':4}
+# A.update({'mango':7})
+# print(A)
+print(A.keys())
+# print(A.__len__())
+# print(A.pop('apple'))
+# print(A)
+# print(A.values())
+
+#print(A.get('apple'))
+
+# arr=("a","b","c","d","e","f")
+# for i in range(0,5):
+#     for j in  range(0,i+1):
+#         print(arr[j],end=" ")
+#     print()
+
+# i=1
+# j=1
+
+# while(i<=5):
+#     while(j):
+#         print(i,j)
+#     i=i+1
+
+# number=int(input("number:"))
+# number2=int(input("number:"))
+# if (number>number2):
+#     print("possitive")
+# elif (number==number2):
+#     print("equal")
+# else:
+#     print("negative")
+# a=lambda x:x*2
+# print(a(2))
+# print(a)
+# b= lambda x,y:x*y
+# print(b(2,3))
+# print(b)
+# l=[5,4,3,2,6,1]
+# import functools
+# final=(lambda x:x*4,l))
+# print(final)
+# l=(100,9999)
+# import random
+# print(random.shuffle(l))
+# print(l)
+# a=int(input())
+# b=int(input())
+# c=int(input())
+
+# if a
+# arr=[1,2,3,4,5]
+# i=0
+# total=0
+# while i<len(arr):
+#     total+=arr[i]
+#     i+=1
+# print(total)
+
+# class scope:
+#     def fun(self,a,b):
+#         print(a+b)
+
+# o=scope()
+# o.fun(2,3)
+        
+
+# class scope:
+#     def __init__(self):
+#         print("constructor called")
+#     def fun1(self):
+#         print("hello")
+# obj=scope()
+# obj.fun1()
+# class person:
+#     def __init__(self):
+#         self.name='sam'
+#         self.age=25
+#         print(self.name,self.age)
+# o=person()
+
+
+
+
+
+# a=int(input("enter a number ?"))
+# b=int(input("enter a number ?"))
+# c=int(input("enter a number ?"))
+# if (a>b&c):
+#     print("a greater")
+# elif (b<c&a):
+#     print("b greater")
+# elif (c>a&b):
+#     print("c greater")
+# else :
+#     print(False)
+
+# T.A THISON 9
+# j.M FEFFIN JO 5
+
+
+
